b410c5afd06ce08e1416e76427904beccda2018d
Miguel <miguelaugl@outlook.com>
2022-02-12 17:02:19 -0300

    feat: add AbstractFactory example

---
+++ src/structural/creational/abstract-factory/View.test.ts
@@ -0,0 +1,17 @@
+import DarkThemeFactory from "./DarkThemeFactory"
+import LightThemeFactory from "./LightThemeFactory"
+import View from "./View"
+
+it('should create a light theme view', () => {
+  const view = new View(new LightThemeFactory())
+  expect(view.label.color).toBe('black')
+  expect(view.button.color).toBe('white')
+  expect(view.button.backgroundColor).toBe('blue')
+})
+
+it('should create a dark theme view', () => {
+  const view = new View(new DarkThemeFactory())
+  expect(view.label.color).toBe('white')
+  expect(view.button.color).toBe('white')
+  expect(view.button.backgroundColor).toBe('black')
+})
