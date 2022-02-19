58d970322985b50b1db6bf058810699f24e86c2c
Miguel <miguelaugl@outlook.com>
2022-02-18 22:44:48 -0300

    feat: add Chain of Responsability example

---
+++ src/behavioral/chain-of-responsability/Atm.ts
@@ -0,0 +1,11 @@
+import BillHandler from "./BillHandler";
+
+export default class Atm {
+	constructor (readonly billHandler: BillHandler) {}
+
+	withdraw (amount: number) {
+		const bills: any[] = [];
+		this.billHandler.handle(bills, amount);
+		return bills;
+	}
+}
---
+++ src/behavioral/chain-of-responsability/BillHandler.ts
@@ -0,0 +1,4 @@
+export default interface BillHandler {
+  setNext(handler: BillHandler): BillHandler;
+	handle(bills: any[], amount: number): void;
+}
